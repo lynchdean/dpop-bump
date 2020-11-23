a2ad8cf45a82baa8d9e8c84fa6f8b5433047d2b8
Dean <lynchdean@gmail.com>
2020-11-23 22:01:14 +0000

    Get list of product elements

--- src/main.py
+++ src/main.py
@@ -1,31 +1,48 @@
 import time
 
+import selenium
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 
 from src import secrets
 
 base_url = "https://www.depop.com/"
+username = secrets.username
 
 
 def bump():
-
     driver = webdriver.Chrome(ChromeDriverManager().install())
 
-    login(driver)
+    # login(driver)
 
+    # time.sleep(2)  # Need to wait or else you are logged back out.
+    driver.get(base_url + username)
     time.sleep(2)
-    driver.get(base_url + secrets.username)
-    # time.sleep(2000)
+
+    products = []
+    i = 1
+    while i < 100:
+        try:
+            print(driver.find_element_by_xpath(f'//*[@id="products-tab"]/div/ul/li[{i}]/a/div/img'))
+            products.append(driver.find_element_by_xpath(f'//*[@id="products-tab"]/div/ul/li[{i}]/a/div/img'))
+            i += 1
+        except NoSuchElementException:
+            break
+
+
+
+
+    print(products)
 
 
 def login(driver):
     driver.get(base_url + "login")
-    driver.find_element_by_xpath('//*[@id="username"]')\
-        .send_keys(secrets.username)
-    driver.find_element_by_xpath('//*[@id="password"]')\
+    driver.find_element_by_xpath('//*[@id="username"]') \
+        .send_keys(username)
+    driver.find_element_by_xpath('//*[@id="password"]') \
         .send_keys(secrets.password)
-    driver.find_element_by_xpath('//*[@id="main"]/div[1]/form/button')\
+    driver.find_element_by_xpath('//*[@id="main"]/div[1]/form/button') \
         .click()
 
 
